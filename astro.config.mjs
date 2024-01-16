c0753884186f20587ebfdbbf9bfbbd401970ccd7
Funasitien DreamCloud <86372093+Funasitien@users.noreply.github.com>
2024-01-16 09:01:02 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
 	integrations: [tailwind()],
 	site: 'https://f.dreamclouds.fr',
-        base: '/astroclouds',
+        base: '/AstroClouds',
 });
